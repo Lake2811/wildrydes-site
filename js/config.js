7b2fb646d848f8e35639b2f348878347852b733e
Lake2811 <minhhoangho29@gmail.com>
2025-04-17 17:43:35 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://en0cqp0z97.execute-api.ap-southeast-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://au7f5609a7.execute-api.ap-southeast-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
